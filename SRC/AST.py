0cabf6f3585cfa282d7870ab9cbb61597c1cc6ea
roflolilolmao <quentin.jeanmonod@he-arc.ch>
2015-12-19 20:58:15 +0100

    STARTED WORKING ON FUNCTIONS

--- SRC/AST.py
+++ SRC/AST.py
@@ -1,14 +1,14 @@
 # coding: latin-1
 
-''' Petit module utilitaire pour la construction, la manipulation et la 
+""" Petit module utilitaire pour la construction, la manipulation et la
 représentation d'arbres syntaxiques abstraits.
 
-Sûrement plein de bugs et autres surprises. À prendre comme un 
+Sûrement plein de bugs et autres surprises. À prendre comme un
 "work in progress"...
 Notamment, l'utilisation de pydot pour représenter un arbre syntaxique cousu
 est une utilisation un peu "limite" de graphviz. Ça marche, mais le layout n'est
 pas toujours optimal...
-'''
+"""
 
 import pydot
 
@@ -103,6 +103,30 @@ class ProgramNode(Node):
     type = 'PROGRAM'
 
 
+class AssignFunctionNode(Node):
+    type = 'ASSIGNFUNCTION'
+
+    def __init__(self, name, child, args=None):
+        Node.__init__(self)
+        self.name = name
+        self.child = child
+        self.args = args
+
+    def __repr__(self):
+        return 'ASSIGN {}'.format(self.name)
+
+
+class FunctionNode(Node):
+    type = 'FUNCTION'
+
+    def __init__(self, name):
+        Node.__init__(self)
+        self.name = name
+
+    def __repr__(self):
+        return 'FUNC {}'.format(self.name)
+
+
 class TokenNode(Node):
     type = 'TOKEN'
 
@@ -147,17 +171,17 @@ class EntryNode(Node):
 
 
 def addToClass(cls):
-    ''' Décorateur permettant d'ajouter la fonction décorée en tant que méthode
+    """ Décorateur permettant d'ajouter la fonction décorée en tant que méthode
     à une classe.
-    
+
     Permet d'implémenter une forme élémentaire de programmation orientée
     aspects en regroupant les méthodes de différentes classes implémentant
     une même fonctionnalité en un seul endroit.
-    
+
     Attention, après utilisation de ce décorateur, la fonction décorée reste dans
     le namespace courant. Si cela dérange, on peut utiliser del pour la détruire.
     Je ne sais pas s'il existe un moyen d'éviter ce phénomène.
-    '''
+    """
     def decorator(func):
         setattr(cls, func.__name__, func)
         return func
